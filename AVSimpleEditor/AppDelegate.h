ede59cfc3c13e2412ba25728e52f0273eeb0d5ec
dbk1985 <376958755@qq.com>
2018-03-23 17:23:57 +0800

    no message

---
+++ AVSimpleEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVSimpleEditor
+//
+//  Created by wzkj on 2018/3/23.
+//  Copyright © 2018年 Cloudwave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVSimpleEditor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVSimpleEditor
+//
+//  Created by wzkj on 2018/3/23.
+//  Copyright © 2018年 Cloudwave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
