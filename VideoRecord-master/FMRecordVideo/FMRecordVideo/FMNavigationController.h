4e025c37b45c372cc7f70efb389764cdf9d89224
dbk1985 <376958755@qq.com>
2018-04-17 11:04:37 +0800

    video record best Orientation

---
+++ VideoRecord-master/FMRecordVideo/FMRecordVideo/FMNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  FMNavigationController.h
+//  FMRecordVideo
+//
+//  Created by wzkj on 2018/3/29.
+//  Copyright © 2018年 SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMNavigationController : UINavigationController
+
+@end
---
+++ VideoRecord-master/FMRecordVideo/FMRecordVideo/FMNavigationController.m
@@ -0,0 +1,42 @@
+//
+//  FMNavigationController.m
+//  FMRecordVideo
+//
+//  Created by wzkj on 2018/3/29.
+//  Copyright © 2018年 SF. All rights reserved.
+//
+
+#import "FMNavigationController.h"
+
+@interface FMNavigationController ()
+
+@end
+
+@implementation FMNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+//
+//-(BOOL)shouldAutorotate
+//{
+//    return NO;
+//}
+//
+//- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+//{
+//    return UIInterfaceOrientationMaskPortrait;
+//}
+//// Returns interface orientation masks.
+//- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+//{
+//    return UIInterfaceOrientationPortrait;
+//}
+
+@end
